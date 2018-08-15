6121da5de93f1733234db1280ab7a6272f1dd2f1
karolinneczka <okaa93@gmail.com>
2018-08-15 23:49:37 +0200

    Add devtools

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -3,10 +3,14 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import {getCountries} from './actions/actions-countries';
+import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <div>
+            <h1>Inicjalizacja projektu</h1>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
