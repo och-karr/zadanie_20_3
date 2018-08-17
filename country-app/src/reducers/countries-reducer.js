aab0097993657a96f8aa7eca4dc6de835302d17e
karolinneczka <okaa93@gmail.com>
2018-08-17 18:28:59 +0200

    Add corrections

--- country-app/src/reducers/countries-reducer.js
+++ country-app/src/reducers/countries-reducer.js
@@ -1,14 +1,12 @@
 import { GET_COUNTRIES } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
-const initialState = {
-    countries: []
-};
+const initialState = [];
 
 const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_COUNTRIES:
-            return Object.assign({}, state, {countries: [state.countries]})
+            return countriesData;
     }
 
     return state;
