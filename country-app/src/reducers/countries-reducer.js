3cdb5d26d3622fc627120042462b9d0c400de1c5
karolinneczka <okaa93@gmail.com>
2018-08-20 14:22:37 +0200

    20.5. First commit something is wrong with props

--- country-app/src/reducers/countries-reducer.js
+++ country-app/src/reducers/countries-reducer.js
@@ -1,12 +1,28 @@
-import { GET_COUNTRIES } from '../actions/actions-countries';
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
-const initialState = []; //generujemy pustą tablicę
+const initialState = 
+//[]; generujemy pustą tablicę
+    {
+        countries: countriesData,
+        selectedCountry: {},
+        visibleCountries: []
+    };
 
 const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_COUNTRIES:
-            return countriesData; //to z importuczyli cała ta tablica
+            //return countriesData; //to z importu czyli cała ta tablica
+            return Object.assign({}, state, {countries: state.countries});
+        
+        case GET_COUNTRY:
+            const selectedCountry = state.countries.find(country => country.id===action.id);
+            return Object.assign({}, state, {selectedCountry});
+
+        case SEARCH_COUNTRIES:
+            const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
+            return Object.assign({}, state, {visibleCountries: foundCountries});
+            
         default:
             return state;
     }
