8cbdd810389d8b231c58ddd433fd758dfdec2f34
karolinneczka <okaa93@gmail.com>
2018-08-20 10:37:49 +0200

    20.4. First commit

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import Navigation from './presentational/navigation.component';
 import {getCountries} from './actions/actions-countries';
+import routes from './routes';
 import DevTools from './DevTools';
+import './country.css';
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes}/>
             <DevTools />
         </div>
     </Provider>,
